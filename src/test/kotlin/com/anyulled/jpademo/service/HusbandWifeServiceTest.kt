69d24638698960fb30bf01f708ab1d88aa3692ba
alrs <anyul.rivas@gft.com>
2018-08-21 12:16:32 +0200

    - service tests (increasing coverage)

--- src/test/kotlin/com/anyulled/jpademo/service/HusbandWifeServiceTest.kt
+++ src/test/kotlin/com/anyulled/jpademo/service/HusbandWifeServiceTest.kt
@@ -17,42 +17,56 @@ internal class HusbandWifeServiceTest {
     private lateinit var husbandWifeService: HusbandWifeService
 
     @Test
-    fun saveWife() {
+    fun `save wife`() {
         val persistedWife = Wife(1L, "wife", null)
         given(husbandWifeService.saveWife(Wife(0, "wife", null))).willReturn(persistedWife)
         assertThat(husbandWifeService.saveWife(Wife(0, "wife", null))).isEqualTo(persistedWife)
     }
 
     @Test
-    fun saveHusband() {
+    fun `save wife with husband`() {
+        val wifeWithHusband = Wife(1L, "wife with husband", Husband(2L, "husband", null))
+        given(husbandWifeService.saveWife(wifeWithHusband)).willReturn(wifeWithHusband)
+        assertThat(husbandWifeService.saveWife(wifeWithHusband)).isEqualTo(wifeWithHusband)
+    }
+
+    @Test
+    fun `save husband`() {
         val persistedHusband = Husband(1L, "husband", null)
         given(husbandWifeService.saveHusband(Husband(0, "husband", null))).willReturn(persistedHusband)
         assertThat(husbandWifeService.saveHusband(Husband(0, "husband", null))).isEqualTo(persistedHusband)
     }
 
     @Test
-    fun getHusbands() {
+    fun `save husband with wife`() {
+        val husbandWithWife = Husband(1L, "husband with wife", Wife(2L, "wife", null))
+        given(husbandWifeService.saveHusband(husbandWithWife)).willReturn(husbandWithWife)
+        assertThat(husbandWifeService.saveHusband(husbandWithWife)).isEqualTo(husbandWithWife)
+    }
+
+    @Test
+    fun `get husbands`() {
         val husbands = listOf(Husband(1L, "Husband 1", null), Husband(2L, "husband 2", null))
         given(husbandWifeService.getHusbands()).willReturn(husbands)
         assertThat(husbandWifeService.getHusbands()).isEqualTo(husbands)
     }
 
     @Test
-    fun getWives() {
+    fun `get wives`() {
         val wives = listOf(Wife(1L, "Wife 1", null), Wife(2L, "Wife 2", null))
         given(husbandWifeService.getWives()).willReturn(wives)
         assertThat(husbandWifeService.getWives()).isEqualTo(wives)
     }
 
     @Test
-    fun getHusband() {
+    fun `get husband`() {
         val expectedHusband = Husband(1L, "husband", null)
         given(husbandWifeService.getHusband(ArgumentMatchers.anyLong())).willReturn(expectedHusband)
         assertThat(husbandWifeService.getHusband(1)).isEqualTo(expectedHusband)
     }
 
     @Test
-    fun getWife() {
+    fun `get wife`() {
         val expectedWife = Wife(1L, "Wife", null)
         given(husbandWifeService.getWife(ArgumentMatchers.anyLong())).willReturn(expectedWife)
         assertThat(husbandWifeService.getWife(1)).isEqualTo(expectedWife)
